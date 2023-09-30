e21a3481ec03124e4d3eb6997c9ade242507de44
S4MU3LPENA <102234459+S4MU3LPENA@users.noreply.github.com>
2023-09-30 07:32:05 -0400

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,17 +1,74 @@
 from sqlalchemy import Column, Table
 from sqlalchemy.sql.sqltypes import Integer, String, DECIMAL
 from config.db import meta, engine
+from sqlalchemy import Table, ForeignKey, Column, Integer, Date, String
 
-users = Table(
-    "users",
+#TABLA CUSTOMER
+
+customer = Table(
+    "customer",
     meta,
-    Column("id", Integer, primary_key=True),
+    Column("customer_id", Integer, primary_key=True),
     Column(
         "name",
         String(255),
     ),
-    Column("username", String(255)),
-    Column("password", String(255)),
+    Column("email", String(255)),
+    Column("address", String(255)),
+)
+
+#TABLA ORDER
+
+order = Table(
+    "order",
+    meta,
+    Column("order_id", Integer, primary_key=True),
+    Column(
+        "order_date",
+        Date,  #si quieres ponle tipo date
+    ),
+    Column("delivery_id", Integer,ForeignKey("delivery.id")),
+    Column("customer_id", Integer,ForeignKey("delivery.id")),
+)
+
+#TABLA PRODUCTS
+
+products = Table(
+    "products",
+    meta,
+    Column("product_id", Integer, primary_key=True),
+    Column(
+        "product_name",
+        String(300),
+    ),
+    Column("category_id", Integer,ForeignKey("category.id")),
+)
+
+#TABLA CATEGORIES
+
+categories = Table(
+    "categories",
+    meta,
+    Column("category_id", Integer, primary_key=True),
+    Column(
+        "category_name",
+        String(255),
+    ),
+    Column("category_type", String(255)),
+    Column("field", String(255), unique=True),
+)
+
+#TABLA DELIVERY
+
+delivery = Table(
+    "delivery",
+    meta,
+    Column("delivery_id", Integer, primary_key=True),
+    Column(
+        "type",
+        String(255),
+    ),
+    Column("status", String(255)),
 )
 
 
